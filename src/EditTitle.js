90f30b24dc5141887239b1ebbe6522fdf75b922c
Himanshu kumar <himanshu28051997@gmail.com>
2019-12-05 10:46:21 +0530

    Minor Changes

--- src/EditTitle.js
+++ src/EditTitle.js
@@ -6,9 +6,14 @@ const EditTitle = (props) =>{
     const handleClick = (e) =>{
         props.editedTitle(title);
     }
+    const handleEvent = (e) =>{
+        if(e.keyCode===13){
+            props.editedTitle(title);
+        }
+    }
     return(
         <div className='et-titleWrapper'>
-            <input type='text' placeholder='Edit Title' defaultValue={props.data} onChange={(e) => setTitle(e.target.value)} className='et-titleList'></input>
+            <input type='text' placeholder='Edit Title' defaultValue={props.data} onChange={(e) => setTitle(e.target.value)} className='et-titleList' onKeyDown={handleEvent}></input>
             <div className='et-btnWrapper'><button className='et-titleSaveButton' onClick={handleClick}>&#x27A4;</button></div>
         </div>
     )
--- src/Title.js
+++ src/Title.js
@@ -6,9 +6,14 @@ const Title = (props) =>{
     const handleClick = (e) =>{
         props.changeTitle(title);
     }
+    const handleEvent = (e) =>{
+        if(e.keyCode===13){
+            props.changeTitle(title);
+        }
+    }
     return(
         <div className='titleWrapper'>
-            <input type='text' placeholder='Enter list title' onChange={(e) => setTitle(e.target.value)} className='titleList'></input>
+            <input type='text' placeholder='Enter list title' onChange={(e) => setTitle(e.target.value)} className='titleList' onKeyDown={handleEvent}></input>
             <div className='titlebtnWrapper'>
                 <button className='titleSaveButton' onClick={handleClick}>&#x27A4;</button>
             </div>
